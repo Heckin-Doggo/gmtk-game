534595331d2a89be23f2321199684cd87dfc1d61
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-06-11 15:38:49 -0400

    peg signal

--- game/scenes/Peg.gd
+++ game/scenes/Peg.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+signal peg_clicked
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,5 +17,6 @@ func _on_Peg_input_event(viewport, event, shape_idx):
 		print("it work")
 		if $AnimatedSprite.animation == "off":
 			$AnimatedSprite.animation = "on"
+			emit_signal("peg_clicked", Vector2(position.x, position.y))
 		else:
 			$AnimatedSprite.animation = "off"
