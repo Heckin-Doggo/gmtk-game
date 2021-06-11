a77738feaa31b67c385c3e5b35723b9a151a77d2
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-06-11 16:03:49 -0400

    meh

--- game/Player.gd
+++ game/Player.gd
@@ -6,25 +6,27 @@ var velocity = Vector2.ZERO
 #the directions of the strings
 var webhooks = []
 
+
 func _physics_process(delta):
+	velocity = Vector2.ZERO
+	
 	#loops over each webhook
 	for webhook in webhooks:
 		#Gets the end of the strings position and the vector to that position
 		var hook_point = webhook.points[1]
-		var pull = hook_point - position
-		#adds that vector to the velocity
-		velocity = velocity + pull
+		velocity += webhook.position
 	
+	print(velocity)
 	set_linear_velocity(velocity)
 
-#Adds a line2d webhook and adds it to the array of current webhooks
-func shoot_web(hook_position):
-	var new_webhook = Web.instance()
-	new_webhook.points[1] = hook_position
-	add_child(new_webhook)
-	webhooks.append(new_webhook)
 
+func new_hook(pos):
+	var new_hook = Web.instance()
+	new_hook.points[1] = Vector2(pos.x - position.x, pos.y - position.y)
+	add_child(new_hook)
+	webhooks.append(new_hook)
+	
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			shoot_web(event.position)
+			new_hook(event.position)
