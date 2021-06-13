e2a78e74c19652ac36f51fac489931df24f2dfa4
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-06-13 11:58:42 -0400

    you can now select 6 + music

--- game/ui/LevelSelect.gd
+++ game/ui/LevelSelect.gd
@@ -6,7 +6,7 @@ extends Control
 func _ready():
 #	for button in $NinePatchRect/VBoxContainer/LevelButtons.get_children():
 #		button.connect("pressed", self, "select_level")
-	for i in range(1,6):
+	for i in range(1,7):
 		var x = str(i)
 		get_node("NinePatchRect/VBoxContainer/LevelButtons/Level"+x).connect("pressed", self, "select_"+x)
 	$NinePatchRect/VBoxContainer/TutorialButton.connect("pressed", self, "select_tutorial")
@@ -27,6 +27,9 @@ func select_4():
 	
 func select_5():
 	get_tree().change_scene("res://levels/Level5.tscn")
+
+func select_6():
+	get_tree().change_scene("res://levels/Level6.tscn")
 
 func select_tutorial():
 	get_tree().change_scene("res://levels/Tutorial.tscn")
